a03ddb008a1b48593b269f4621bd9a8d41b85b0e
herosea.zhou <herosea.zhou@gmail.com>
2016-08-18 15:26:03 +0800

    添加spock测试代码

---
+++ src/test/groovy/me/ysee/study/controllers/PeopleControllerSpecification.groovy
@@ -0,0 +1,61 @@
+package me.ysee.study.controllers
+
+import me.ysee.study.StudySpringbootApplication
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import spock.lang.Shared
+import spock.lang.Specification
+
+import static org.hamcrest.core.IsEqual.equalTo
+import static org.hamcrest.Matchers.containsString
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by herosea on 16/8/18.
+ */
+@SpringBootTest
+class PeopleControllerSpecification extends Specification {
+
+    @Shared
+    boolean sharedSetupDone = false;
+    @Shared
+    private MockMvc mockMvc;
+
+    void setup() {
+        if (!sharedSetupDone){
+            mockMvc = MockMvcBuilders.standaloneSetup(new PeopleController()).build();
+            sharedSetupDone = true;
+        }
+    }
+
+    def "Index"() {
+        when:
+        def result = mockMvc.perform(get("/people").accept(MediaType.APPLICATION_JSON))
+
+        then:
+        result.andExpect(status().isOk())
+        result.andExpect(content().string(containsString("用户1")))
+    }
+
+    def "Fetch"() {
+        when:
+        def result = mockMvc.perform(get("/people/${id}").accept(MediaType.APPLICATION_JSON))
+
+        then:
+        result.andExpect(status().isOk())
+        result.andExpect(content().string(equalTo(json)))
+
+        where:
+        id  | json
+        100 | "{\"name\":\"名字\",\"sex\":true,\"age\":100}"
+        110 | "{\"name\":\"名字\",\"sex\":true,\"age\":110}"
+    }
+}
